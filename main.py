050ad93b7c36585198a7ec9c8e91b081b9ac5645
Wojciech Trojanowski <wjf.trojanowski@gmail.com>
2024-11-13 20:48:22 +0100

    Initial template for html created

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from openai import OpenAI
 
 def generate_image(prompt, index):
     response = client.images.generate(
-        model="dall-e-2",
+        model="dall-e-3",
         prompt=prompt,
         n=1,
-        size="512x512"
+        size="1024x1024"
     )
     image_url = response.data[0].url
     
@@ -25,10 +25,10 @@ messages=[
     {
         "role": "user", 
         "content": f"""Please convert the following text into an HTML document:\n\n{text}. 
-        Return only the code between <body> and </body> tags (exlusive). With use of <img> tags with an atribute `src=\"image_placeholder.jpg\"`. 
+        Return only the code between <body> and </body> tags (do not innclude <body> tags as well). 
+        Try to find appropriate places for images in the text and add <img> tags with atribute `src=\"image_placeholder.jpg\"` in those places. 
         Add attribute `alt` to each image with a prompt instruction which we can later use to generate those graphics. For example: \"IMAGE 1: Generate an image of a voice assistant\".
-        Try to signify that is the <alt> tag with a prompt and not a part of the text.
-        Use <br>, <center> and <em> tags for caption of the images. Center the images as well.
+        Use <br>, <center> and <em> tags for caption of the images (avoid simple statements: "Ilustration of..."). Center the images as well. Scale the images to 512x512.
         """
     }
 ]
@@ -38,10 +38,14 @@ completion = client.chat.completions.create(
 )
 
 html_code = completion.choices[0].message.content.replace("```html", "").replace("```", "").strip()
+output_html_path = "artykul.html"
+with open(output_html_path, 'w') as html_file:
+    html_file.write(html_code)
+print(f"Initial {output_html_path} saved with placeholders.")
 
 prompts = []
 while True:
-    prompt = input("Enter a prompt (or press Enter to finish)>> ")
+    prompt = input("Enter a prompt (or press Enter to finish) >>> ")
     if not prompt:
         break
     prompts.append(prompt)
@@ -49,11 +53,6 @@ for index, prompt in enumerate(prompts):
     image_path = generate_image(prompt, index)
     html_code = html_code.replace(f'src="image_placeholder.jpg"', f'src="{image_path}"', 1)
 
-for index, prompt in enumerate(prompts):
-    image_path = generate_image(prompt, index)
-    html_code = html_code.replace(f'src="image_placeholder.jpg"', f'src="{image_path}"', 1)
-
-output_html_path = "artykul.html" 
 with open(output_html_path, 'w') as html_file:
     html_file.write(html_code)
 print(f"{output_html_path} saved.")
